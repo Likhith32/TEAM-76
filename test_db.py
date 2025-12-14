8a8d8a710460d996e41b3824e6ef9a5094e39e2b
Likhith32 <likhithmanakala@gmail.com>
2025-12-14 10:04:45 +0530

    Delete test_db.py

--- test_db.py
+++
@@ -1,8 +0,0 @@
-from config.supabase import engine
-
-try:
-    conn = engine.connect()
-    print("✅ Connected to Supabase PostgreSQL successfully")
-    conn.close()
-except Exception as e:
-    print("❌ Connection failed:", e)
